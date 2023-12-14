84cfcc37105eebb55a493cb45ab1c67a5cf640f6
abhi-jha12 <jhaabhishek887@gmail.com>
2023-12-14 22:36:47 +0530

    added auth

--- src/utils/SignInSignUpModal.jsx
+++ src/utils/SignInSignUpModal.jsx
@@ -1,10 +1,42 @@
-// SignInSignUpModal.jsx
 import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
+import { auth } from '../firebaseConfig'; // Adjust if necessary
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import ImageCard2 from './ImageCard2'; // Adjust the path to your ImageCard component
 
 const SignInSignUpModal = ({ onClose }) => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    country: '',
+    phoneNumber: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password, firstName, lastName, country, phoneNumber } = formData;
+
+    try {
+      if (isSignUp) {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        // Add additional user information to your database if needed
+        // For example, save firstName, lastName, country, phoneNumber to Firestore
+      } else {
+        await signInWithEmailAndPassword(auth, email, password);
+      }
+      onClose(); // Close the modal on successful sign in/sign up
+    } catch (error) {
+      // Handle errors here, such as displaying a notification
+      console.error("Authentication error:", error.message);
+    }
+  };
 
   const toggleForm = () => {
     setIsSignUp(!isSignUp);
@@ -21,53 +53,81 @@ const SignInSignUpModal = ({ onClose }) => {
           <button onClick={onClose} className="absolute top-0 right-0 m-4 text-black text-2xl">
             <FaTimes />
           </button>
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             {isSignUp ? (
               // Sign-up form structure
               <>
                 <div className="flex space-x-2">
                   <input
                     type="text"
+                    name="firstName"
                     placeholder="First name"
                     className="w-1/2 px-4 py-2 border-2 border-black rounded-lg"
+                    onChange={handleChange}
+                    value={formData.firstName}
                   />
                   <input
                     type="text"
+                    name="lastName"
                     placeholder="Surname"
                     className="w-1/2 px-4 py-2 border-2 border-black rounded-lg"
+                    onChange={handleChange}
+                    value={formData.lastName}
                   />
                 </div>
                 <input
                   type="email"
+                  name="email"
                   placeholder="Email address"
                   className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                  onChange={handleChange}
+                  value={formData.email}
                 />
                 <div className="flex space-x-2">
                   <input
                     type="text"
+                    name="country"
                     placeholder="Country"
                     className="w-1/3 px-4 py-2 border-2 border-black rounded-lg"
+                    onChange={handleChange}
+                    value={formData.country}
                   />
                   <input
                     type="tel"
+                    name="phoneNumber"
                     placeholder="Phone number"
                     className="w-2/3 px-4 py-2 border-2 border-black rounded-lg"
+                    onChange={handleChange}
+                    value={formData.phoneNumber}
                   />
                 </div>
-                {/* Add additional sign-up fields here */}
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
               </>
             ) : (
               // Sign-in form structure
               <>
                 <input
                   type="text"
+                  name="email"
                   placeholder="Email address or phone number"
                   className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                  onChange={handleChange}
+                  value={formData.email}
                 />
                 <input
                   type="password"
+                  name="password"
                   placeholder="Password"
                   className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                  onChange={handleChange}
+                  value={formData.password}
                 />
                 <div className="text">
                   <a href="#" className="text-sm text-gray-600 hover:underline">
@@ -77,12 +137,12 @@ const SignInSignUpModal = ({ onClose }) => {
               </>
             )}
             <div className="flex justify-center items-center h-full">
-            <button
-              type="submit"
-              className="w-auto bg-black text-white px-4 py-2 rounded-lg"
-            >
-              {isSignUp ? 'Sign Up' : 'Login'}
-            </button>
+              <button
+                type="submit"
+                className="w-auto bg-black text-white px-4 py-2 rounded-lg"
+              >
+                {isSignUp ? 'Sign Up' : 'Login'}
+              </button>
             </div>
           </form>
           <div className="text-center mt-6">
